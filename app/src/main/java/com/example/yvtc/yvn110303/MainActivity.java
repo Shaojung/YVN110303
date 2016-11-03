91d59c02f66d7fd0e43e681ee01b2b1dd206831c
shaojung <shaojung@gmail.com>
2016-11-03 19:43:52 +0800

    add onItemClickListener

--- app/src/main/java/com/example/yvtc/yvn110303/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvn110303/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.yvtc.yvn110303;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
                                 fruits);
         lv.setAdapter(adapter);
 
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, fruits[position], Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
